951a1ba8e35a2e6605f6723f4981f90648c25f22
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-12-21 19:50:45 +0600

    task add

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ async function run() {
 
 
         app.delete('/taskdelete/:id', async (req, res) => {
+            
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await taskCollection.deleteOne(query);
